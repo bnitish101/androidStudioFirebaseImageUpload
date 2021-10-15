4abbefd400834c5b39f3100ebd55d9388d999f8c
Nitish <bnitish101@gmail.com>
2021-10-15 21:49:10 +0530

    permission dependency added to read external storage

--- app/src/main/java/com/example/androidstudiofirebaseimageupload/MainActivity.java
+++ app/src/main/java/com/example/androidstudiofirebaseimageupload/MainActivity.java
@@ -2,13 +2,60 @@ package com.example.androidstudiofirebaseimageupload;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.DexterActivity;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
+    Button btnBrowse, btnUpload;
+    Uri filePath;
+    Bitmap bitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageView = (ImageView) findViewById(R.id.imageView);
+        btnBrowse = (Button) findViewById(R.id.btnBrowse);
+        btnUpload = (Button) findViewById(R.id.btnUpload);
+
+        btnBrowse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Dexter.withContext(MainActivity.this)
+                        .withPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                        .withListener(new PermissionListener() {
+                            @Override
+                            public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+
+                            }
+
+                            @Override
+                            public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+
+                            }
+
+                            @Override
+                            public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+
+                            }
+                        }).check();
+            }
+        });
     }
 }
